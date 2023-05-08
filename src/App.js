671209a30122f286bb47cabf2fe41c8af3d20c36
lovephimu <soe.nlb@gmail.com>
2023-05-08 22:19:34 +0200

    Delete unused files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import Countdown from './Countdown';
-import styles from './index.module.scss';
-import logo from './logo.svg';
 
 export default function App() {
   return <Countdown />;
--- src/Countdown.js
+++ src/Countdown.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.scss';
 
 export default function Countdown() {
